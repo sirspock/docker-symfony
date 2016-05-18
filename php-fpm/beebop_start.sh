7076ba5f3a451befebf155dcd6f94e1eb3633462
Maximiliano <mosorio@linets.cl>
2016-05-18 16:11:52 -0400

    sacar mysql

--- php-fpm/beebop_start.sh
+++ php-fpm/beebop_start.sh
@@ -4,12 +4,11 @@ cd /var/www/symfony
 
 php app/console doctrine:database:create --if-not-exists; 
 if [ $? -ne 0 ]; then
-	echo "new"
 	php app/console doctrine:schema:create;
 	php app/console doctrine:fixtures:load --fixtures=src/AppBundle/DataFixtures/ORMProd/
 	app/console app:assets --env=prod
 fi
-echo "Actualizacion"
+
 php app/console doctrine:schema:update --dump-sql --force
 app/console app:assets --env=prod
 php app/console ca:c --env=prod
